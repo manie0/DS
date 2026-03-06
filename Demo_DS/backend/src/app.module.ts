6d5730497f0d2ed90299930352911904bf8d6647
Jose Manuel Ferrer Vidal <jjferrer0202@gmail.com>
2026-03-06 17:14:33 -0500

    Add /api root endpoint and document it

--- Demo_DS/backend/src/app.module.ts
+++ Demo_DS/backend/src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { ObserverModule } from './observer/observer.module';
 import { SingletonController } from './singleton/singleton.controller';
+import { RootController } from './root.controller';
 import { SingletonService } from './singleton/singleton.service';
 
 @Module({
   imports: [ObserverModule],
-  controllers: [SingletonController],
+  controllers: [RootController, SingletonController],
   providers: [PrismaService, SingletonService],
 })
 export class AppModule {}
